ae7f47c7a3e8df817b80831a810ab554477c5d83
Clay Raynor <cd.raynor@gmail.com>
2021-08-08 12:13:36 -0400

    Add methods to schema

--- logzio/resource_s3_bucket_connection.go
+++ logzio/resource_s3_bucket_connection.go
@@ -1,1 +1,34 @@
 package logzio
+
+import "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+
+func resourceS3BucketConnector() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceS3BucketConnectorCreate,
+		Read: resourceS3BucketConnectorRead,
+		Update: resourceS3BucketConnectorUpdate,
+		Delete: resourceS3BucketConnectorDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+
+		},
+	}
+}
+
+func resourceS3BucketConnectorCreate(d *schema.ResourceData, m interface{}) error {
+
+}
+
+func resourceS3BucketConnectorRead(d *schema.ResourceData, m interface{}) error {
+
+}
+
+func resourceS3BucketConnectorUpdate(d *schema.ResourceData, m interface{}) error {
+
+}
+
+func resourceS3BucketConnectorDelete(d *schema.ResourceData, m interface{}) error {
+
+}
